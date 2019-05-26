a44cfec04c8845f0fa0e09b39246313405e043c2
AlexMcLean123 <alimclean22@gmail.com>
2019-05-26 13:37:27 +0100

    updates

--- demo/src/main/java/com/example/demo/controller/Controller.java
+++ demo/src/main/java/com/example/demo/controller/Controller.java
@@ -3,9 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.genre.Genre;
 import com.example.demo.service.genreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,6 +18,17 @@ public class Controller {
 
     @RequestMapping("/genres")
     public List<Genre> getAllGenres() {
-    return myService.getAllGenres();
+        return myService.getAllGenres();
+    }
+
+    @RequestMapping("/genres/{type}")
+    public Genre getGenre(@PathVariable String type) {
+        return myService.getGenre(type);
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/genres")
+    public void addGenre(@RequestBody Genre genre){
+        myService.addGenre(genre);
     }
 }
--- demo/src/main/java/com/example/demo/service/genreService.java
+++ demo/src/main/java/com/example/demo/service/genreService.java
@@ -1,20 +1,39 @@
 package com.example.demo.service;
 
 import com.example.demo.genre.Genre;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @org.springframework.stereotype.Service
 public class genreService {
 
-    public List<Genre> getAllGenres() {
-        return Arrays.asList(
-                new Genre("Techno", 10),
-                new Genre("Country", 4),
-                new Genre("Classical", 2),
-                new Genre("Dance", 6),
-                new Genre("Rock", 8));
+    private List<Genre> genres = new  ArrayList<> (Arrays.asList(
+            new Genre("Techno", 10),
+            new Genre("Country", 4),
+            new Genre("Classical", 2),
+            new Genre("Dance", 6),
+            new Genre("Rock", 8),
+            new Genre("Jazz", 6),
+            new Genre("Pop", 8),
+            new Genre("Hip Hop", 9)));
 
+
+    public List<Genre> getAllGenres() {
+        return genres;
     }
+
+    public Genre getGenre(String type) {
+        return genres.stream().filter(t -> t.getType().equalsIgnoreCase(type)).findFirst().get();
+    }
+
+    public void addGenre(Genre genre){
+        genres.add(genre);
+    }
+
+
 }
